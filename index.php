bd70ff4ff443602adba5dbd76235efbf38d84553
FranciscoD20010 <fj.dp21@gmaiil.com>
2022-09-15 10:03:16 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="public/css/main.css">
+
+    </head>
+    <body>
+        <div class="container">
+            <div class="container2">
+                <form>
+                    <fieldset>
+                        <legend>
+                            Datos de accesso
+                        </legend>
+                        <label>
+                            Email:
+                        </label>
+                        <input type="text" name="" placeholder="write here">
+
+                        <label>
+                            Password:
+                        </label>
+                        <input type="password" placeholder="******" name="password">
+
+                        <button type="submit">
+                            Acceder
+                        </button>
+                        
+                    </fieldset>
+                </form>
+            </div>
+        </div>
+        
+    </body>
+</html>
