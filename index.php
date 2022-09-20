128950ee9a7b0d2b5c882a19b8ae553d80063773
FranciscoD20010 <fj.dp21@gmaiil.com>
2022-09-20 10:02:33 -0600

    Clase de bootstrap

--- index.php
+++ index.php
@@ -2,34 +2,38 @@
 <html>
     <head>
         <link rel="stylesheet" href="public/css/main.css">
-
-    </head>
+        <!-- CSS only -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">    </head>
     <body>
-        <div class="container">
-            <div class="container2">
-                <form>
+        <div class="col-sm-6 text-black">
+            <div class="d-flex align-items-center h-custom-2 px-5 ms-xl-4 mt-5 pt-5 pt-xl-0 mt-xl-n5">
+                <form style="width: 23rem;">
                     <fieldset>
-                        <legend>
+                        <legend class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">
                             Datos de accesso
                         </legend>
                         <label>
                             Email:
                         </label>
-                        <input type="text" name="" placeholder="write here">
-
+                        <div>
+                        <input type="text" name="" class="form-control form-control-lg" placeholder="Write here">
+                        </div>
+                        <div class="mb-6">
                         <label>
                             Password:
                         </label>
-                        <input type="password" placeholder="******" name="password">
-
-                        <button type="submit">
+                        <input type="password" class="form-control form-control-lg" placeholder="*********">
+                        </div>        
+                        <div class="pt-1 mb-4" style="text-align: center" >
+                        <button type="button" class="btn btn-info btn-lg btn-block">
                             Acceder
-                        </button>
-                        
+                        </button> 
+                        </div>               
                     </fieldset>
                 </form>
             </div>
         </div>
-        
+        <!-- JavaScript Bundle with Popper -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
     </body>
 </html>
