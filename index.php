d57f0569045ac63616810151435de9dda8981da4
Francisco Diaz <fj.dp21@gmail.com>
2022-09-27 13:32:37 -0500

    Login, falta hacer la la vista de detalle y cortar la plantilla en secciones para ser invocadas :(

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <body>
         <div class="row justify-content-md-center">
             <div class="col-md-3 col-lg-3 col-sm-12" style="padding-top: 15%;">
-                <form method="get" action="products" class="form" style="width: 23rem;">
+                <form method="post" action="app/AuthController.php" class="form" style="width: 23rem;">
                     <fieldset>
                         <legend class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">
                             Datos de accesso
@@ -16,15 +16,16 @@
                             Email:
                         </label>
                         <div>
-                        <input type="text" name="" class="form-control form-control-lg" placeholder="Write here">
+                        <input type="text" name="email" class="form-control form-control-lg" placeholder="Write here">
                         </div>
                         <div class="mb-6">
                         <label>
                             Password:
                         </label>
-                        <input type="password" class="form-control form-control-lg" placeholder="*********">
+                        <input type="password" name="password" class="form-control form-control-lg" placeholder="*********">
                         </div>        
                         <div class="pt-1 mb-4" style="text-align: center" >
+                        <input type="hidden" name="action" value="access">
                         <button type="submit" class="btn btn-info btn-lg btn-block">
                             Acceder
                         </button> 
